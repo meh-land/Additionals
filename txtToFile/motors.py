0e258de91db9d182040e2126dda3ea74c0301e9b
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-06-14 11:09:23 +0200

    random numnbers for motors velocoties

---
+++ txtToFile/motors.py
@@ -0,0 +1,24 @@
+import random
+import time
+
+
+def Logs(filename):
+    while True:
+        try:
+            m1 = random.randint(1, 100)  
+            m2 = random.randint(1, 100)  
+            m3 = random.randint(1, 100)
+            m4 = random.randint(1, 100)  
+            
+            with open(filename, 'a') as file:
+                file.write(f"{m1}, {m2}, {m3}, {m4}\n")
+            
+            print(f"Successfully appended {m1, m2, m3, m4} to {filename}.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        
+        time.sleep(10)
+
+# change path to be storage\app in dashboard backend  directory
+filename = "C:\\xampp\\htdocs\\dashboard_ros\\storage\\app\\Motors.txt"
+Logs(filename)
